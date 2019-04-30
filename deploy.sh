74d0de2cc72151f68fdc3d0922132ea82bd3661d
chenkan <chenkan@zbj.com>
2019-04-30 18:39:51 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://${access_token}@github.com/isChenKan/isChenKan.github.io.git master
+git push -f https://${chenkan}@github.com/isChenKan/isChenKan.github.io.git master
 
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
