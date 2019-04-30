7f3e01320dc48a92aa64b2ace3796f291e64f9de
chenkan <chenkan@zbj.com>
2019-04-30 18:03:17 +0800

    自动部署

---
+++ .vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	title: '🌻葵花养殖技术人员',
+	description: '前端技术博客',
+	// dest: './public', // 设置输出目录
+	base: '/', // 设置站点根路径
+	markdown: {
+		lineNumbers: true
+	},
+	themeConfig: {
+    sidebar: [
+			'/',
+			'/20190430-sort/',
+			'/20190430-tree/',
+			'/20190430-mysql/',
+			'/20190430-carousel/',
+		],
+		sidebarDepth: 2,
+	},
+}
