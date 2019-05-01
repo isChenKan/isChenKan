34c13a34c6d6991331ee1fe21360bffb3dfc677d
chenkan <chenkan@zbj.com>
2019-05-01 13:42:53 +0800

    :tada: 新增部署文档

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,17 @@ module.exports = {
 	description: '前端技术博客',
 	// dest: './public', // 设置输出目录
 	base: '/', // 设置站点根路径
+	themeConfig: {
+		sidebar: [
+			'/',
+			'/20190430-sort/',
+			'/20190430-tree/',
+			'/20190430-mysql/',
+			'/20190430-carousel/',
+			'/20190501-deploy/',
+		],
+		sidebarDepth: 2,
+	},
 	head: [
 		['link', { rel: 'icon', href: '/icon.ico' }],
 		['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
@@ -10,16 +21,6 @@ module.exports = {
 	],
 	markdown: {
 		lineNumbers: true
-	},
-	themeConfig: {
-    sidebar: [
-			'/',
-			'/20190430-sort/',
-			'/20190430-tree/',
-			'/20190430-mysql/',
-			'/20190430-carousel/',
-		],
-		sidebarDepth: 2,
 	},
 	serviceWorker: true,
 }
